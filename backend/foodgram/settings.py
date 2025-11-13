60cf7a37e3c4fec5ba08a2c246e7322307732a83
Trifonov Evgeny <evtrifonov@yandex-team.ru>
2025-11-13 17:04:46 +0300

    Fixed docker-compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # CSRF_TRUSTED_ORIGINS = [
 #     "http://localhost:8000",
