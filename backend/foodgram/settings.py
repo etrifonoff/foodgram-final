3c56661f954273a249170029609f24c972076c11
Trifonov Evgeny <evtrifonov@yandex-team.ru>
2025-11-14 11:44:09 +0300

    Fixed docker-compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
